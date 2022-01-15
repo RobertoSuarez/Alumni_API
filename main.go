6f0b1af2e5c50f54212e7f5782262423d79411f5
RobertoSuarez <electrosonix12@gmail.com>
2022-01-14 22:14:45 -0500

    vue build mounted

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.AutomaticEnv()
+	viper.SetDefault("port", "3000")
+
+	app := fiber.New()
+
+	app.Static("/", "./dist")
+
+	log.Println("listen to :" + viper.GetString("port"))
+	app.Listen(":" + viper.GetString("port"))
+}
