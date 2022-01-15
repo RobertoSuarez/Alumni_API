810c60ce88675d3ce13e15a8da1df079ff801b2c
RobertoSuarez <electrosonix12@gmail.com>
2022-01-15 16:51:45 -0500

    frontend actualizado y setiado para tomar rutas *

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/spf13/viper"
@@ -13,7 +14,20 @@ func main() {
 
 	app := fiber.New()
 
-	app.Static("/*", "./dist")
+	app.Static("/", "./dist", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		Browse:        true,
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+	})
+	app.Static("/*", "./dist", fiber.Static{
+		Compress:      true,
+		ByteRange:     true,
+		Browse:        true,
+		CacheDuration: 10 * time.Second,
+		MaxAge:        3600,
+	})
 
 	log.Println("listen to :" + viper.GetString("port"))
 	app.Listen(":" + viper.GetString("port"))
